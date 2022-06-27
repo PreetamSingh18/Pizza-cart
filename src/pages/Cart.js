32d01819668810a00a6fe2487834c197a3e148b8
Preetam Singh <spreetam2001@gmail.com>
2022-06-27 21:03:31 +0530

    proxy change

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -19,7 +19,7 @@ const Cart = () => {
       return;
     }
 
-    fetch('/api/products/cart-items', {
+    fetch('https://ecom-rest-apis.herokuapp.com/api/products/cart-items', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
